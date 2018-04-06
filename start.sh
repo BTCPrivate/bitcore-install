b6eb0a1c7d942a4b900d130f4e5527004d612564
Jon Layton <me@jonl.io>
2018-04-06 17:35:53 +0000

    v5 setup+run instructions clarified

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion 
 
 # Start Bitcore Services (and Daemon)
-nvm use v9 #v4
+# For >= 5.0 (bitpay website instructions)
+nvm use v9; bitcored
 
-./node_modules/bitcore-node/bin/bitcore-node start
-# (run this from where you ran `bitcore create x` (btcp-bitcore-node))
-# (or, if set up like the >= 5.0 bitpay website instructions, just `bitcored`)
+# For v3.1: cd btcp-explorer; nvm use v4; ./node_modules/bitcore-node/bin/bitcore-node start
+
